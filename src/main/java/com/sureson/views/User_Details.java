5f2afc85e89c2fc114a6a998a9e170869a5461d2
kushankavinda <kaavinda92@gmail.lk>
2017-12-27 00:06:39 +0530

    connecting front end and back end
    Signed-off-by: kushankavinda <kaavinda92@gmail.lk>

--- src/main/java/com/sureson/views/User_Details.java
+++ src/main/java/com/sureson/views/User_Details.java
@@ -1,16 +1,28 @@
 package com.sureson.views;
 
+import java.io.File;
+
 import com.sureson.controllers.Controll_Registered_users;
 
 public class User_Details{
 	public static void main(String args[]) {
+		/*Controll_Registered_users  controll_Registered_users  = new Controll_Registered_users ();
+		String a= controll_Registered_users.getUser(1);
+		System.out.println("view is coming to the party baby : "+a);*/
+	//	System.out.println("k8ush");
+		User_Details a =new User_Details();
+		/*ClassLoader classLoader = a.getClass().getClassLoader();
+		File file = new File(classLoader.getResource("User_Details").getFile());
+		System.out.println(file.getAbsolutePath());*/
+		System.out.println(a.getClass().getClassLoader().getResourceAsStream("myFeatures.properties"));
+		
+	}	
+	public static void testing() {
+		System.out.println("tesing okay");	
 		Controll_Registered_users  controll_Registered_users  = new Controll_Registered_users ();
 		String a= controll_Registered_users.getUser(1);
 		System.out.println("view is coming to the party baby : "+a);
-	//	System.out.println("k8ush");
-	}
-	public static void testing() {
-		System.out.println("tesing okay");	
+		//this.getClass().getClassLoader().getResourceAsStream("myFeatures.properties");
 	}
 }
 

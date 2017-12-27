f4e6194a9aff66d5fd961593f717a9d0f872b4bd
kushankavinda <kaavinda92@gmail.lk>
2017-12-27 18:00:36 +0530

    # WARNING: head commit changed in the meantime
    before connect rest ans clint side

---
+++ src/main/java/restApi/connectClient.java
@@ -0,0 +1,16 @@
+package restApi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/connect")
+public class connectClient {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getUsers() {
+		return "kushan is unstoppable";
+	}
+}
+
